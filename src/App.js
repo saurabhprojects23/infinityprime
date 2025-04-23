4fa227f87c7217ac55f2e7417c7f537a1504139b
saurabhprojects23 <findjob.saurabh@gmail.com>
2025-04-23 17:01:59 +0530

    secure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage/>} />
-      </Routes>
-    </Router>
+   <>
+   <LandingPage/>
+   </>
   );
 }
 
